55e2910cce055e84fd91335199423880af3d7d74
Sandro Shubitidze <s.shubitidze@andersenlab.com>
2026-02-08 14:15:22 +0400

    [SP-002] add match get and post endpoints

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import express from 'express'
+import { matchRouter } from './routes/matches.js';
+
 
 const app = express();
 const port = 8000;
+
 app.use(express.json())
 
 app.get('/', (req,res) => {
   res.send('Hello from Express server!')
 })
+app.use('/matches', matchRouter)
+
 
 app.listen(port, () => {
   console.log(`Server is running...`)
