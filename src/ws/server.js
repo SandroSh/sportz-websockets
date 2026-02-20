9b69807a4db73e537fc4479c49f0f6862711306d
Sandro Shubitidze <s.shubitidze@andersenlab.com>
2026-02-20 23:51:05 +0400

    [SP-006] add arcjet

--- src/ws/server.js
+++ src/ws/server.js
@@ -1,4 +1,6 @@
 import { WebSocket, WebSocketServer } from "ws";
+import { wsArcjet } from "../arcjet.js";
+
 
 function sendJson(socket, payload) {
   if (socket.readyState !== WebSocket.OPEN) {
@@ -11,7 +13,7 @@ function sendJson(socket, payload) {
 function broadcast(wss, payload) {
   for (const client of wss.clients) {
     if (client.readyState !== WebSocket.OPEN) {
-      return;
+      continue;
     }
 
     client.send(JSON.stringify(payload));
@@ -25,66 +27,46 @@ export function attachWebSocketServer(server) {
     maxPayload: 1024 * 1024,
   });
 
-  wss.on('connection', (socket) => {
-    sendJson(socket, {type:'welcome'});
+  wss.on("connection", async (socket, req, res) => {
+    if(wsArcjet){
+      try {
+        const decision = await wsArcjet.protect(req)
+        if(decision.isDenied()){
+          const code = decision.reason.isRateLimit() ? 1013 : 1008;
+          const reason = decision.reason.isRateLimit() ? 'Rate limit exceeded' : 'Access denied';
+      
+          socket.close(code, reason)
+          return
+        }
+      } catch (error) {
+        console.error('WS connection error', error);
+        socket.close('1011', 'Server security error')
+        return
+      } 
+    }
 
-    socket.on('error', console.error)
-  })
+    socket.isAlive = true;
+    socket.on("pong", () => {
+      socket.isAlive = true;
+    });
 
-  function broadcastMatchCreated(match){
-    broadcast(wss, {type:'match_created', data: match})
+    sendJson(socket, { type: "welcome" });
+
+    socket.on("error", console.error);
+  });
+
+  const interval = setInterval(() => {
+    wss.clients.forEach((ws) => {
+      if(ws.isAlive === false) return ws.terminate();
+      ws.isAlive = false;
+      ws.ping()
+    })
+  }, 30000)
+
+
+  function broadcastMatchCreated(match) {
+    broadcast(wss, { type: "match_created", data: match });
   }
 
-  return {broadcastMatchCreated}
+  return { broadcastMatchCreated };
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
